3b60ea2d1acb18639e58502063034e044161bd19
Fabricio Henrique <fabriciohod@hotmail.com>
2020-03-28 20:35:20 -0300

    _

--- Q5/Program.cs
+++ Q5/Program.cs
@@ -4,9 +4,25 @@ namespace Q5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int qtd = 0;
+            Console.Write ("Digite um nomero: ");
+            int x = int.Parse (Console.ReadLine ());
+            Console.Write ("Figite um numero: ");
+            int y = int.Parse (Console.ReadLine ());
+            if (x > y)
+            {
+                int aux = 0;
+                aux = x;
+                x = y;
+                y = aux;
+            }
+            while (x < y)
+            {
+                Console.Write ($"{qtd++} ");
+                if (qtd == (y + 1)) break;
+            }
         }
     }
 }
